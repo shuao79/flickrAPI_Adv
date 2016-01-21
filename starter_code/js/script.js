bc1b5989b000078376fb4434e5719b0ac772f6ae
shuao79 <tahirtanzimkhan@mybihs.org>
2016-01-21 22:58:17 +0000

    Fliker

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,12 @@
-// YOUR CODE GOES HERE
+$(document).ready(function(){
+
+$("#search-btn").click(function(){
+var searchvalue = $("#search-query").val();
+  searchFlickr(searchvalue,function(results){
+      for (var m = 0; m < 10; m++) {
+          var image = results[m];
+          $('#search-results').append('<img src="' + image.media.m + '" />');
+      }
+  });
+});
+});
